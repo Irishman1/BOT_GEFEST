006aaaddca15ce59d9fea98ec80bf43759fadff0
Alex <sashaukr1999@gmail.com>
2026-06-08 09:29:45 +0300

    Update parsing interval to 12 hours and stop tracking bot.log

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ SEARCH_URL = "https://gefest.ua/search"
 DB_PATH = os.path.join(os.path.dirname(__file__), "gefest.db")
 
 # Сколько раз в N часов обновлять каталог квартир
-PARSE_INTERVAL_HOURS = 2
+PARSE_INTERVAL_HOURS = 12
 
 CONTACT_TELEGRAM = "@romanova_neruhomist"
 CONTACT_PHONE = "+380930828903"
